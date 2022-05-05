de5db0d03b4fa3fef7c0533cecd10be5ab6176ba
Patrik Beichner <patrik.beichner@gmail.com>
2022-05-05 16:11:46 -0400

    working call to proxy api

--- src/App.js
+++ src/App.js
@@ -4,55 +4,81 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Reports from './pages/Reports'
 import Products from './pages/Products'
-
-//get location via browser geolocation
-  useEffect(() => {
-const controller = new AbortController();
-
-    const fetchData = async () => {
-    if (!navigator.geolocation) {
-      setErrors(errors => [...errors, "Location not supported"]);
-    } else {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          setLat(position.coords.latitude);
-          setLon(position.coords.longitude);
-        });
-
-        await fetch(`${process.env.API_URL_LOCATION}zip=${zip}&country=${country}`)
-          .then(res=>res.json())
-          .then(result => {
-            setCity(result.name)
-            console.log(city)
-          })
-
-      console.log("Latitude is: ", lat)
-      console.log("Longitude is: ", lon)
-      }
-       fetch(`${process.env.API_URL_WEATHER}?lat=${lat}&lon=${lon}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res=>res.json())
-        .then(result => {
-          setForecast(result)
-          console.log(result)
-          console.log(`${process.env.REACT_APP_API_URL}?lat=${lat}&lon=${lon}$units=imperial&APPID=${process.env.REACT_API_KEY}`)
-        })
-      .catch((err) => {
-        if (err.name === 'AbortError') {
-          console.log('successfully aborted');
-        } else {
-          //handle error here
-        }
-      })
-      return () => {
-        controller.abort();
-      }
-      }
-    
-    fetchData();
-    
-  }, [lat, lon]);
+import React, { useEffect, useState } from 'react';
 
 
 function App() {
+
+  let weatherUrl = '/api/weather?';
+
+  const [lat, setLat] = useState('');
+  const [lon, setLon] = useState('');
+  const [errors, setErrors] = useState('');
+  const [zip, setZip] = useState('');
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [forecast, setForecast] = useState('');
+
+//get location via browser geolocation
+useEffect(() => {
+  const controller = new AbortController();
+  
+      const fetchData = async () => {
+      if (!navigator.geolocation) {
+        setErrors(errors => [...errors, "Location not supported"]);
+      } else {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            setLat(position.coords.latitude);
+            setLon(position.coords.longitude);
+          });
+          
+          console.log(`${weatherUrl}latitude=${lat}&longitude=${lon}`);
+          await fetch(`${weatherUrl}latitude=${lat}&longitude=${lon}`, {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+          })
+          .then(res=>res.json())
+          .then(result => {
+            setForecast(result)
+            console.log(result)
+            // console.log(`${process.env.API_URL_WEATHER}latitude=${lat}&longitude=${lon}`)
+          })
+          
+          // await fetch(`${process.env.API_URL_LOCATION}zip=${zip}&country=${country}`)
+          //   .then(res=>res.json())
+          //   .then(result => {
+          //     setCity(result.name)
+          //     console.log(city)
+          //   })
+  
+        console.log("Latitude is: ", lat)
+        console.log("Longitude is: ", lon)
+        }
+        //  fetch(`${process.env.API_URL_WEATHER}latitude=${lat}&longitude=${lon}`)
+        //   .then(res=>res.json())
+        //   .then(result => {
+        //     setForecast(result)
+        //     console.log(result)
+        //     console.log(`${process.env.API_URL_WEATHER}latitude=${lat}&longitude=${lon}`)
+        //   })
+        // .catch((err) => {
+        //   if (err.name === 'AbortError') {
+        //     console.log('successfully aborted');
+        //   } else {
+        //     //handle error here
+        //   }
+        // })
+        return () => {
+          controller.abort();
+        }
+        }
+      
+      fetchData();
+      
+    }, [lat, lon]);
+
   return (
     <>
     <Router>
